05448b01633b93c87cace6007a25a1b814f19c57
Tanuj Tak <tanuj.tak@wwindia.com>
2025-06-18 16:39:46 +0530

    Initial Commit

---
+++ SwiftUIAssesement/SwiftUIAssesementApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAssesementApp.swift
+//  SwiftUIAssesement
+//
+//  Created by tanuj tak on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAssesementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
